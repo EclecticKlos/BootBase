635d99be918f0fbba932c55d5274f221da1714f8
EclecticKlos <dsklos@gmail.com>
2015-03-31 08:22:19 -0700

    basic project object can be created

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -7,17 +7,33 @@ get '/' do
   erb :login
 end
 
-get '/project_list' do
-  erb :project_list
-end
-
 post '/login' do
   if User.find_by(name: params[:name])
-    redirect '/project_list'
+    redirect '/projects'
   else
     "incorrect name"
   end
 end
+
+get '/projects' do
+  @all_projects = Project.all
+
+  erb :projects
+end
+
+get '/projects/new' do
+  erb :projects_new
+end
+
+post '/projects' do
+  new_project = Project.create(title: params[:project_title], description: params[:project_description])
+  if new_project.save
+    redirect '/projects'
+  else
+    "There was an error creating your project"
+  end
+end
+
 
 ########################Implement signup
 # post 'signup' do
--- source/app/models/project.rb
+++ source/app/models/project.rb
@@ -1,3 +1,3 @@
-class User < ActiveRecord::Base
+class Project < ActiveRecord::Base
   belongs_to  :user
 end
