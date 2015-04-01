b4c7255b3247cc54c27d25c7b3303cf038b9aabf
EclecticKlos <dsklos@gmail.com>
2015-03-31 23:14:55 -0700

    Beginning to write Ajax.

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,13 +1,35 @@
-get '/' do
-########## implement with bcrypt
+GET '/' do
+######################## implement with bcrypt
   # if session[:user_id]
   #   erb :project_list
   # else
   #   erb :login
-  erb :login
+  # erb :login
+  erb :git_login
 end
 
-post '/login' do
+######################## Implement signup
+# post 'signup' do
+# end
+
+GET 'git_login' do
+redirect 'https://github.com/login/oauth/authorize'
+
+git_request = .ajax({
+  url:        'https://github.com/login/oauth.authorize'
+  type:       'POST'
+  dataType:   'json'
+  data: {
+    client_id=    '76e09f72b75dbea43129'
+    redirect_uri= 'http://127.0.0.1:9393/projects'
+    scope=        user:email,user:follow,repo,repo_deployment,red:repo_hook
+    state=
+  }
+  })
+
+end
+
+POST '/login' do
   if User.find_by(name: params[:name])
     redirect '/projects'
   else
@@ -15,13 +37,13 @@ post '/login' do
   end
 end
 
-get '/projects' do
+GET '/projects' do
   @all_projects = Project.all
 
   erb :projects
 end
 
-post '/projects' do
+GET '/projects' do
   new_project = Project.create(title: params[:project_title], description: params[:project_description])
   if new_project.save
     redirect '/projects'
@@ -30,11 +52,11 @@ post '/projects' do
   end
 end
 
-get '/projects/new' do
+GET '/projects/new' do
   erb :projects_new
 end
 
-get '/projects/:id' do
+GET '/projects/:id' do
   @this_project = Project.find(params[:id])
   p @this_project
 
@@ -42,11 +64,6 @@ get '/projects/:id' do
 end
 
 
-
-
-########################Implement signup
-# post 'signup' do
-# end
 
 
 
